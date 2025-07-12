bf81f4be76aeac6be7e6741ed71248bfcf098fc6
MariaAntonova4 <maria.an.antonova@gmail.com>
2025-07-12 13:53:28 +0300

    Add Types

---
+++ types.d.ts
@@ -0,0 +1,21 @@
+type SendInfo={
+    num:number
+};
+type Atype={
+    num:number
+    exampleString:string
+};
+type UnsubscribeFunction=()=>void;
+
+interface Window{
+    electron:{
+        subscribeStatistics:(callback:(sendInfo:SendInfo)=>void)=>UnsubscribeFunction;
+        getA:()=>Promise<Atype>;
+    }
+}
+type EventPayloadMapping = {
+  sendInfo: SendInfo;
+  getA: Atype;
+  changeView: View;
+  sendFrameAction: FrameWindowAction;
+};
